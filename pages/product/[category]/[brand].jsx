c9233fa5c209097ff748638714ac10bc9513ab6e
hasti <hasti.kaviyan21poor@gmail.com>
2025-10-03 21:41:42 +0330

    search bar is compelet

--- pages/product/[category]/[brand].jsx
+++ pages/product/[category]/[brand].jsx
@@ -1,12 +1,43 @@
 import { useRouter } from 'next/router'
-
-
+import db from '../../../components/data/db.json'
+import { useState , useEffect } from 'react'
+import ProductCard from '@/components/productcard/productCard'
 const ProductsByBrand = () => {
 
   const {category , brand} = useRouter().query
+  const [laptops , setLaptops] = useState([])
+
+
+
+    useEffect(() => {
+     {
+      setLaptops(getLaptops(brand))
+     
+    }
+  }, [brand])
+
+
+        const getLaptops = (brand) => {
+
+          return db.laptops.filter(laptop => laptop.brand == brand)
+
+         }   
+
+
   return (
     <>
-         <h1>ProductsByBrand - {category} - {brand}</h1>   
+   <div className="container">
+    <div className="section">
+      <h1>laptops- {brand}</h1>
+      <div className="row">
+        {laptops.map(laptop => (
+          <div className="col" key={laptop.id}>
+            < ProductCard {...laptop}/>
+          </div>
+        ))}
+      </div>
+    </div>
+   </div>
     </>
   )
 }
