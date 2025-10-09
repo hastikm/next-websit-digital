a08c858e274a6e4c7671276aca7228965a56683a
hasti <hasti.kaviyan21poor@gmail.com>
2025-10-09 23:55:06 +0330

    first commit

--- components/offcanves/OffCanves.jsx
+++ components/offcanves/OffCanves.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IoCloseSharp } from "react-icons/io5";
+import { IoIosArrowDown } from "react-icons/io";
+import styles from './OffCanves.module.css';
+import Link from 'next/link';
 
 const OffCanves = () => {
+  const [StatusMenue, setStatusMenue] = useState('open') 
+
+  const closeMenue = () => {
+    setStatusMenue('close')
+  }
+
   return (
     <div>
-      <h1>ofcanves page</h1>
-    
+      <aside className={StatusMenue === 'open' ? styles.offCanvasMenu : styles.closeMenu}>
+        <div className={styles.offCanvasHeader}>
+          <h3>مقداد آی تی</h3>
+          <IoCloseSharp size='20px' onClick={closeMenue} />
+        </div>
+        <ul>
+          <li>
+             <Link href='/product/laptops'>لپ تاپ</Link>
+             <IoIosArrowDown />
+          </li>
+          <li>
+             <Link href='/product/mobiles'>موبایل</Link>
+             <IoIosArrowDown />
+          </li>
+          <li>
+             <Link href='/product/camera'>دوربین</Link>
+             <IoIosArrowDown />
+          </li>
+          <li>
+             <Link href='/product/console'>کنسول بازی</Link>
+             <IoIosArrowDown />
+          </li>
+        </ul>
+      </aside>
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
+import OffCanves from "@/components/offcanves/OffCanves";
+
 export default function App({ Component, pageProps }) {
   return (
     <>
     <Header />
     <Component {...pageProps} />
     <Footer />
+    <OffCanves />
     </>
+    
   );
 }
